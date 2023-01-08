9bfd860ff8099736a93c65f3ce5a24102421c41e
visualjeff <visualjeff@users.noreply.github.com>
2023-01-07 20:16:28 -0800

    4 files modified

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,18 @@
-import { Outlet, Link } from 'react-router-dom';
-import { getContacts } from '../contacts';
+import { Form, Outlet, Link, useLoaderData } from 'react-router-dom';
+import { getContacts, createContact } from '../contacts';
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export default function Root() {
+  const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -18,19 +29,33 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
