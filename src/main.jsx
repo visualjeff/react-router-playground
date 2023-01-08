f0396dfc11020052652807833b638e5b97246fa7
visualjeff <visualjeff@users.noreply.github.com>
2023-01-07 19:40:57 -0800

    1 file modified, 1 added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from './routes/contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 
