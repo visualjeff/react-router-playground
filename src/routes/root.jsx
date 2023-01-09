3a769825a2194b07c45544b03dd53f9dbbb5bc0b
visualjeff <visualjeff@users.noreply.github.com>
2023-01-08 19:13:28 -0800

    1 file modified

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,10 @@
-import { Form, Outlet, Link, useLoaderData } from 'react-router-dom';
+import {
+  Form,
+  Outlet,
+  NavLink,
+  useLoaderData,
+  redirect,
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function loader() {
@@ -8,7 +14,7 @@ export async function loader() {
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
@@ -38,7 +44,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -47,7 +58,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
