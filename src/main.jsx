9a146b7e1ae890ff899e6143f47d30afee7989ed
visualjeff <visualjeff@users.noreply.github.com>
2023-01-08 19:39:10 -0800

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,9 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId/destroy',
         action: destroyAction,
+        errorElement: (
+          <div>Oops! There was an error destroying this record.</div>
+        ),
       },
     ],
   },
