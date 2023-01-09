0b1b1a7721921cbe1c23cd3c08668b1fda05e09b
visualjeff <visualjeff@users.noreply.github.com>
2023-01-08 20:18:57 -0800

    1 file modified

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -28,6 +28,10 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
   useEffect(() => {
     setQuery(q);
   }, [q]);
@@ -40,6 +44,7 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? 'loading' : ''}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -50,7 +55,7 @@ export default function Root() {
                 submit(e.currentTarget.form);
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
